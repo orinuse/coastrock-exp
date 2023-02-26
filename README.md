4b44f22cca2ae6fe1952262ca3ed99ab1c326c9d
Orin <73348166+orinuse@users.noreply.github.com>
2023-02-26 18:28:36 +0800

    Repository Menial Tasks
    - Batch File under tools for creating syslinks
    - README and editorconfig
    - Moved comment in template popfile to the bottom

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 None of what's written here is considered final! Anything in this repository is subject to change.
 
-Name: Sunny-Side Up
-[Map](https://wiki.teamfortress.com/wiki/Coastrock): Coastrock
-Difficulty: Expert
-Remarks: Endurance.
+Name: Sunny-Side Up<br>
+[Map](https://wiki.teamfortress.com/wiki/Coastrock): Coastrock<br>
+Difficulty: Expert<br>
+Remarks: Endurance.<br>
---
+++ tools/coast_makelinks.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+TITLE %CD%
+CD /D %~dp0
+
+ECHO ++ coastrock-exp ++
+ECHO ===================
+ECHO/
+
+REM - Check for admin rights.. https://superuser.com/questions/204760/batch-script-how-to-check-for-admin-rights
+REM - - Because links require admin rights. Deleting files don't though..?
+fsutil dirty query %systemdrive% >nul
+IF %errorlevel% EQU 1 (
+	ECHO Run this script with admin rights!
+	PAUSE
+	GOTO :EOF
+)
+
+REM - I really do not like copy-pasting the same file paths a bilion times
+REM - - And for everyone's sake, verify the file paths first
+SET "_PATH_POP=T:\SteamLibrary\steamapps\common\Team Fortress 2\tf\custom\orin\scripts\population"
+SET "_PATH_GIT=D:\Software\GitHub\repos\coastrock-exp\scripts\population"
+IF NOT EXIST "%_PATH_POP%" (
+	SET /P "_PATH_POP=Default path to Team Fortress 2 is incorrect.. Please provide the right path: "
+)
+IF NOT EXIST "%_PATH_GIT%" (
+	SET /P "_PATH_GIT=Default path to GitHib is incorrect.. Please provide the right path: "
+)
+
+mklink "%_PATH_POP%\mvm_coastrock_rc1_1_exp_sunny_side_up.pop" "%_PATH_GIT%\mvm_coastrock_rc1_1_exp_sunny_side_up.pop"
+mklink "%_PATH_POP%\mvm_coastrock_rc1_1_TEST_REMORIN.pop" "%_PATH_GIT%\mvm_coastrock_rc1_1_TEST_REMORIN.pop"
+mklink "%_PATH_POP%\robot_remorin.pop" "%_PATH_GIT%\robot_remorin.pop"
+
+TIMEOUT 10
